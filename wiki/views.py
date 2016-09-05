840a60a640f76e228baa2ae014444fb6b355c2b1
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2016-09-04 22:36:25 -0400

    design details

--- wiki/views.py
+++ wiki/views.py
@@ -10,18 +10,85 @@ class IndexView(generic.ListView):
     queryset = ParaHuman.objects.all()
     template_name_suffix = 'parahuman_list'
 
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Returns a response, using the `response_class` for this
+        view, with a template rendered with the given context.
+        If any keyword arguments are provided, they will be
+        passed to the constructor of the response class.
+        """
+        response_kwargs.setdefault('content_type', self.content_type)
+        context['parahumans'] = True
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
+
 
 class ParaHumanDetailView(generic.DetailView):
     model = ParaHuman
     template_name = 'parahuman.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Returns a response, using the `response_class` for this
+        view, with a template rendered with the given context.
+        If any keyword arguments are provided, they will be
+        passed to the constructor of the response class.
+        """
+        response_kwargs.setdefault('content_type', self.content_type)
+        context['parahumans'] = True
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
 
 
 class EntranceDetailView(generic.DetailView):
     model = Entrance
     template_name = 'entrance.html'
 
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Returns a response, using the `response_class` for this
+        view, with a template rendered with the given context.
+        If any keyword arguments are provided, they will be
+        passed to the constructor of the response class.
+        """
+        response_kwargs.setdefault('content_type', self.content_type)
+        context['entrances'] = True
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
+
 
 class EntrancesListView(generic.ListView):
     model = Entrance
     template_name = 'entrances_list.html'
 
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Returns a response, using the `response_class` for this
+        view, with a template rendered with the given context.
+        If any keyword arguments are provided, they will be
+        passed to the constructor of the response class.
+        """
+        response_kwargs.setdefault('content_type', self.content_type)
+        context['entrances'] = True
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
